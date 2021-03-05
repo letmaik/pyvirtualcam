c5f4683b883e88c145763f6c7acf11c6c67643bf
Maik Riechert <maik.riechert@arcor.de>
2021-03-05 23:44:14 +0000

    ci

--- .github/scripts/install-v4l2loopback.sh
+++ .github/scripts/install-v4l2loopback.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -e -x
+set -x
 
 # Install v4l2loopback kernel module
 sudo apt-get install -y v4l2loopback-dkms
+cat /var/lib/dkms/v4l2loopback/0.10.0/build/make.log
 
 # Create a virtual camera device for the tests
 sudo modprobe v4l2loopback devices=1
