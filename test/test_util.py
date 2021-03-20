9a5df88db5cba1f1114c54d2bb56e3f980164d20
Maik Riechert <maik.riechert@arcor.de>
2021-03-20 09:08:55 +0000

    ci: disable fpscounter test on macOS

--- test/test_util.py
+++ test/test_util.py
@@ -1,6 +1,12 @@
+import os
+import platform
 import time
+import pytest
 import pyvirtualcam.util
 
+@pytest.mark.skipif(
+    os.environ.get('CI') and platform.system() == 'Darwin',
+    reason='disabled due to high fluctuations in CI, manually verified on MacBook Pro')
 def test_fps_counter():
     target_fps = 20
     s_per_frame = 1 / target_fps
