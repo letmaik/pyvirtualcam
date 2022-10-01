a48fd91e6b19499accbdc38b07a1a4bb5f018b7d
Maik Riechert <maik.riechert@arcor.de>
2022-10-01 19:23:39 +0100

    test against obs 28.0.2
    Signed-off-by: Maik Riechert <maik.riechert@arcor.de>

--- .github/scripts/install-macos-obs-virtual-cam.sh
+++ .github/scripts/install-macos-obs-virtual-cam.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e -x
 
-VERSION="28.0.1"
+VERSION="28.0.2"
 
-#DMG_FILENAME="obs-mac-${VERSION}.dmg"
 DMG_FILENAME="obs-studio-${VERSION}-macos-x86_64.dmg"
 DMG_URL="https://cdn-fastly.obsproject.com/downloads/${DMG_FILENAME}"
-#MOUNT_PATH="/Volumes/OBS-Studio ${VERSION}"
 MOUNT_PATH="/Volumes/OBS-${VERSION}-macOS-Intel"
 INSTALL_PATH="/Library/CoreMediaIO/Plug-Ins/DAL"
 
@@ -15,6 +13,5 @@ sudo mkdir -p "${INSTALL_PATH}"
 curl -L --retry 3 "${DMG_URL}" -o "${DMG_FILENAME}"
 
 sudo hdiutil attach "${DMG_FILENAME}"
-#sudo cp -r "${MOUNT_PATH}/OBS.app/Contents/resources/data/obs-plugins/mac-virtualcam/obs-mac-virtualcam.plugin" "${INSTALL_PATH}"
 sudo cp -r "${MOUNT_PATH}/OBS.app/Contents/resources/obs-mac-virtualcam.plugin" "${INSTALL_PATH}"
 sudo hdiutil unmount "${MOUNT_PATH}" -force
