1bca5cf2ac25d3432ffc79befcb4277ec9a2ce77
Maik Riechert <maik.riechert@arcor.de>
2024-02-08 23:04:51 +0000

    fix

--- .github/scripts/install-macos-obs-virtual-cam.sh
+++ .github/scripts/install-macos-obs-virtual-cam.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e -x
 
-if [[ "$ARCH" == "arm64" ]]; then
-    echo "arm64 not supported for live testing"
-    exit 0
-fi
-
 VERSION="28.0.2"
 
-DMG_FILENAME="obs-studio-${VERSION}-macos-x86_64.dmg"
+if [[ "$ARCH" == "arm64" ]]; then
+    ARCH_MOUNT="Apple"
+else
+    ARCH_MOUNT="Intel"
+fi
+
+DMG_FILENAME="obs-studio-${VERSION}-macos-${ARCH}.dmg"
 DMG_URL="https://cdn-fastly.obsproject.com/downloads/${DMG_FILENAME}"
-MOUNT_PATH="/Volumes/OBS-${VERSION}-macOS-Intel"
+MOUNT_PATH="/Volumes/OBS-${VERSION}-macOS-${ARCH_MOUNT}"
 INSTALL_PATH="/Library/CoreMediaIO/Plug-Ins/DAL"
 
 sudo mkdir -p "${INSTALL_PATH}"
@@ -18,5 +19,6 @@ sudo mkdir -p "${INSTALL_PATH}"
 curl -L --retry 3 "${DMG_URL}" -o "${DMG_FILENAME}"
 
 sudo hdiutil attach "${DMG_FILENAME}"
+ls -l "/Volumes"
 sudo cp -r "${MOUNT_PATH}/OBS.app/Contents/resources/obs-mac-virtualcam.plugin" "${INSTALL_PATH}"
 sudo hdiutil unmount "${MOUNT_PATH}" -force
